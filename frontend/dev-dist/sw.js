01d83d6d1d7e8614866ee580c2ece8369a92357d
YASMENMAD <yasmineedaoudii@gmail.com>
2026-06-30 09:36:05 +0100

    Configure base path and GitHub Action workflow for automatic GitHub Pages deployment

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-970124e6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.e0mp2njr0i"
+    "revision": "0.53a9mtotuo4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
